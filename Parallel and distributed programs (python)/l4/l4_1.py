a91f67361ed6bd0d2ec627740633ddd88fdc83e2
Pur3Vessel <pur3vessel@mail.ru>
2022-12-04 21:54:01 +0300

    add raw l4

---
+++ Parallel and distributed programs (python)/l4/l4_1.py
@@ -0,0 +1,57 @@
+import threading
+from time import sleep
+
+n = 10
+s = []
+r = []
+
+class MyThread(threading.Thread):
+    def __init__(self, name):
+        super().__init__(name=name)
+
+    def run(self):
+        if self.name == 'A':
+            print('A start')
+            for i in range(n):
+                s.append(i)
+                sleep(1)
+            print('A finish')
+        if self.name == 'B':
+            print('B start')
+            for i in range(n):
+                if len(s) == 0:
+                    sleep(1)
+                else:
+                    t = s.pop()
+                    r.append(t * t)
+            print('B finish')
+        if self.name == 'C':
+            print('C start')
+            for i in range(n):
+                if len(s) == 0:
+                    sleep(1)
+                else:
+                    t = s.pop()
+                    r.append(t / 3)
+            print('C finish')
+        if self.name == 'D':
+            print('D start')
+            for i in range(n):
+                if len(r) == 0:
+                    print('In R there are not elements')
+                    sleep(1)
+                else:
+                    print(r.pop())
+            print('D finish')
+
+
+A = MyThread('A')
+B = MyThread('B')
+C = MyThread('C')
+D = MyThread('D')
+
+threads = [A, B, C, D]
+for thread in threads:
+    thread.start()
+for thread in threads:
+    thread.join()
